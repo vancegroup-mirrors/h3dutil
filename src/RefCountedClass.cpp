086d00eb42a68530a24888440134a53b1b552af4
markus <markus@875a7764-6f23-0410-b93c-dbbdadb1ea73>
2007-03-16 15:52:27 +0000

    Moved all threads except HLThread to H3DUtil. Also added MutexLock to RefCountedClass and created constructors to create a lock in case it is needed. ( only used in HAPIHapticShape so far ). This is a temporary ugly fix, should be changed.
    git-svn-id: https://www.h3d.org:8090/H3DUtil/trunk/H3DUtil@10 875a7764-6f23-0410-b93c-dbbdadb1ea73

--- src/RefCountedClass.cpp
+++ src/RefCountedClass.cpp
@@ -1,2 +1,71 @@
 
 #include "RefCountedClass.h"
+
+using namespace H3DUtil;
+
+RefCountedClass::RefCountedClass( ):
+      ref_count( 0 ),
+      name( "" ),
+      type_name( "RefCountedClass" ),
+      is_initialized( false ),
+      manual_initialize( false ),
+      use_ref_count_lock( false ),
+      ref_count_lock_pointer(0) {
+}
+
+RefCountedClass::RefCountedClass( bool _use_lock ):
+      ref_count( 0 ),
+      name( "" ),
+      type_name( "RefCountedClass" ),
+      is_initialized( false ),
+      manual_initialize( false ),
+      use_ref_count_lock( _use_lock ){
+  if( _use_lock ) {
+    ref_count_lock_pointer = new MutexLock();
+    ref_count_lock_pointer->lock();
+    ref_count_lock_pointer->unlock();
+  }
+}
+
+RefCountedClass::~RefCountedClass() {
+#ifdef REF_COUNT_DEBUG
+  Console(1) << "~RefCountedClass: " << this << endl;
+#endif
+  if( use_ref_count_lock ) {
+    delete ref_count_lock_pointer;
+  }
+}
+
+void RefCountedClass::ref() {
+  if( use_ref_count_lock )
+    ref_count_lock_pointer->lock();
+  ref_count++;
+#ifdef REF_COUNT_DEBUG
+  Console(1) << "Ref " << getName() << " " << this << ": " 
+    << ref_count << endl;
+#endif
+  if( !manual_initialize && ref_count == 1 ) {
+    initialize();
+  }
+  if( use_ref_count_lock )
+    ref_count_lock_pointer->unlock();
+}
+
+void RefCountedClass::unref() {
+  if( use_ref_count_lock )
+    ref_count_lock_pointer->lock();
+  ref_count--;
+#ifdef REF_COUNT_DEBUG
+  Console(1) << "Unref " << getName() << " " << this << ": " 
+    << ref_count << endl;
+#endif
+  if( ref_count == 0 ) {
+    if( use_ref_count_lock )
+      ref_count_lock_pointer->unlock();
+    delete this;
+  }
+  else {
+    if( use_ref_count_lock )
+      ref_count_lock_pointer->unlock();
+  }
+}

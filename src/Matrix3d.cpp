46f0516c702b3ed3b68e932f185a68f6d5df6113
markus <markus@875a7764-6f23-0410-b93c-dbbdadb1ea73>
2009-03-24 10:03:10 +0000

    getScalePart can now return negative scale when the scale is negative.
    git-svn-id: https://www.h3d.org:8090/H3DUtil/trunk/H3DUtil@124 875a7764-6f23-0410-b93c-dbbdadb1ea73

--- src/Matrix3d.cpp
+++ src/Matrix3d.cpp
@@ -176,7 +176,13 @@ ostream& H3DUtil::ArithmeticTypes::operator<<( ostream &os, const Matrix3d &m ) 
 }
 
 Vec3d Matrix3d::getScalePart() const {
-  return Vec3d( ( (*this) * Vec3d(1,0,0) ).length(),
-                ( (*this) * Vec3d(0,1,0) ).length(),
-                ( (*this) * Vec3d(0,0,1) ).length() );
+  Vec3d x_axis = (*this) * Vec3d(1,0,0);
+  Vec3d y_axis = (*this) * Vec3d(0,1,0);
+  Vec3d z_axis = (*this) * Vec3d(0,0,1);
+  return Vec3d( x_axis * Vec3d( 1, 0, 0 ) < 0 ?
+                  -x_axis.length() : x_axis.length(),
+                y_axis * Vec3d( 0, 1, 0 ) < 0 ?
+                  -y_axis.length() : y_axis.length(),
+                z_axis * Vec3d( 0, 0, 1 ) < 0 ?
+                  -z_axis.length() : z_axis.length() );
 }
